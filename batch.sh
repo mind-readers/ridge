afbe2ba24aeea5ae1d919f2bd1846c9182f646fd
Nicholas Herson <nicholas.herson@gmail.com>
2014-11-02 18:08:23 -0600

    adding a sample batch script to issue a job to SLURM using MPI

---
+++ batch.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+#SBATCH --job-name=mpi-test
+#SBATCH --output=mpi-test.log
+#
+#SBATCH --ntasks=8
+#SBATCH --cpus-per-task=8
+#SBATCH --time=10:00
+
+# The above configs do the following
+# 	job-name: some human-readable string for a name
+#	output: Place where all of STDOUT+STDERR goes
+#	ntasks: Tells SLURM the number of instances of this procedure are needed
+#		By default, each instance of the procedure is allocated a single processor
+#		If each task is itself multithreaded, then this will not do, and you
+#		should also specify 'cpus-per-task'
+#	cpus-per-task: The number of processors needed to compute one task.
+#		So if each task does something like set OMP_NUM_THREADS=8,
+#		then this option should probably be set to 8
+#	time: The time until a task is killed due to timeout 
+
+
+# Weird round-about method of loading modules on 
+# stampede that allows for proper mpi4py usage
+# Will probably be different in GallantLab
+module load python/2.7.3-epd-7.3.2   # this is the older one...
+module load mpi4py                   # a second step is needed
+module unload python
+module load intel/14.0.1.106
+module load python/2.7.6  
+
+
+# use ibrun on stampede in order to use MPI
+# this will need to change when running in GallantLab
+ibrun python test.py  
