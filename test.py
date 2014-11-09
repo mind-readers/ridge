82a0d2aa97edc1f4ab56776c01005ba884f7a9a1
gelvin <gelvin@outlook.com>
2014-11-09 14:33:46 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import numpy as np
 import logging
+from ridge_test import ridge_test, ridge_corr_test, bootstrap_ridge_test
 from ridge import ridge, ridge_corr, bootstrap_ridge
 
 logging.basicConfig(level=logging.DEBUG)
@@ -23,10 +24,40 @@ Rstim = features[:TR]
 Pstim = features[TR:]
 
 # Run bootstrap ridge
+wt_test, corr_test, valphas_test, bscorrs_test, valinds_test = bootstrap_ridge_test(Rstim, Rresp, Pstim, Presp,
+                                                      alphas=np.logspace(-2, 2, 20),
+                                                      nboots=5,
+                                                      chunklen=10, nchunks=15)
+
 wt, corr, valphas, bscorrs, valinds = bootstrap_ridge(Rstim, Rresp, Pstim, Presp,
                                                       alphas=np.logspace(-2, 2, 20),
                                                       nboots=5,
                                                       chunklen=10, nchunks=15)
+
+for wt_cur in wt_test:
+    if wt_cur not in wt:
+        print "fail in wt"
+        return
+
+for corr_cur in corr_test:
+    if corr_cur not in corr:
+        print "fail in corr"
+        return
+
+for valphas_cur in valphas_test:
+    if valphas_cur not in valphas:
+        print "fail in valphas"
+        return
+
+for bscorrs_cur in bscorrs_test:
+    if bscorrs_cur not in bscorrs:
+        print "fail in bscorrs"
+        return
+
+for valinds_cur in valinds_test:
+    if valinds_cur not in valinds:
+        print "fail in valinds"
+        return
 
 
 # Corr should increase quickly across "voxels". Last corr should be large (>0.9-ish).
