5fd82e76bd09b6a83ac45cf087f838efd3606106
Nicholas Herson <nicholas.herson@gmail.com>
2014-11-18 04:39:49 -0600

    use numpy.allclose for testing instead

--- test.py
+++ test.py
@@ -23,13 +23,9 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
-#if rank==0:
 realwt = np.random.randn(N, M)
 features = np.random.randn(TR+TP, N)
 noise = np.random.randn(TR+TP, M)
-#    comm.bcast((realwt, features, noise), root=0)
-#else:
-#    realwt, features, noise = comm.bcast(None, root=0)
 
 
 snrs = np.linspace(0, 0.2, M)
@@ -52,60 +48,54 @@ wt, corr, valphas, bscorrs, valinds = bootstrap_ridge(Rstim, Rresp, Pstim, Presp
                                                       nboots=12,
                                                       chunklen=10, nchunks=15, test_bootstrap=True)
 
-for wt_cur in wt_test:
-   if wt_cur not in wt:
-       print "fail in wt"
-       exit(1)
+#for i in range(wt.shape[0]):
+#    for j in range(wt.shape[1]):
+#        if wt[i][j] != wt_test[i][j]:
+#            print "fail in wt: " + str(wt[i][j]) + "!=" + str(wt_test[i][j]) + " at " + str(i) + ", " + str(j)
+#            exit(1)
 
-for i in range(wt.shape[0]):
-    for j in range(wt.shape[1]):
-        if wt[i][j] != wt_test[i][j]:
-            print "fail in wt: " + str(wt[i][j]) + "!=" + str(wt_test[i][j]) + " at " + str(i) + ", " + str(j)
-            exit(1)
+if not np.allclose(wt, wt_test):
+    print("Fail in wt")
+    exit(1)
 
-#for corr_cur in corr_test:
-#    if corr_cur not in corr:
+#for i in range(corr.shape[0]):
+#    if corr[i] != corr_test[i]:
 #        print "fail in corr"
 #        exit(1)
 
-for i in range(corr.shape[0]):
-    if corr[i] != corr_test[i]:
-        print "fail in corr"
-        exit(1)
+if not np.allclose(corr, corr_test):
+    print("Fail in corr")
+    exit(1)
 
-#for valphas_cur in valphas_test:
-#    if valphas_cur not in valphas:
+#for i in range(valphas.shape[0]):
+#    if valphas[i] != valphas_test[i]:
 #        print "fail in valphas"
 #        exit(1)
 
-for i in range(valphas.shape[0]):
-    if valphas[i] != valphas_test[i]:
-        print "fail in valphas"
-        exit(1)
+if not np.allclose(valphas, valphas_test):
+    print("Fail in valphas")
+    exit(1)
 
+#for i in range(bscorrs.shape[0]):
+#    for j in range(bscorrs.shape[1]):
+#        for k in range(bscorrs.shape[2]):
+#            if bscorrs[i][j][k] != bscorrs_test[i][j][k]:
+#                print "fail in bootstrap_corrs"
+#                exit(1)
 
-#for bscorrs_cur in bscorrs_test:
-#    if bscorrs_cur not in bscorrs:
-#        print "fail in bscorrs"
-#        exit(1)
+if not np.allclose(bscorrs, bscorrs_test):
+    print("Fail in bscorrs")
+    exit(1)
 
-for i in range(bscorrs.shape[0]):
-    for j in range(bscorrs.shape[1]):
-        for k in range(bscorrs.shape[2]):
-            if bscorrs[i][j][k] != bscorrs_test[i][j][k]:
-                print "fail in bootstrap_corrs"
-                exit(1)
+#for i in range(valinds.shape[0]):
+#    for j in range(valinds.shape[1]):
+#        if valinds[i][j] != valinds_test[i][j]:
+#            print "fail in valinds"
+#            exit(1)
 
-#for valinds_cur in valinds_test:
-#    if valinds_cur not in valinds:
-#        print "fail in valinds"
-#        exit(1)
-
-for i in range(valinds.shape[0]):
-    for j in range(valinds.shape[1]):
-        if valinds[i][j] != valinds_test[i][j]:
-            print "fail in valinds"
-            exit(1)
+if not np.allclose(valinds, valinds_test):
+    print("Fail valinds")
+    exit(1)
 
 
 #Corr should increase quickly across "voxels". Last corr should be large (>0.9-ish).
